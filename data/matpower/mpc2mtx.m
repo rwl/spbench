9ea90f1b185d18ec472c43df249588fd210110f5
Richard Lincoln <r.w.lincoln@gmail.com>
2022-10-29 11:34:08 +0200

    Slicing reference bus rows/columns out of Bbus matrices to make them solve.

--- data/matpower/mpc2mtx.m
+++ data/matpower/mpc2mtx.m
@@ -17,6 +17,16 @@ mmwrite(Yname, Ybus);
 
 Bbus = makeBdc(mpc.baseMVA, mpc.bus, mpc.branch);
 
+
+[PQ, PV, REF, NONE, BUS_I, BUS_TYPE, PD, QD, GS, BS, BUS_AREA, VM, ...
+    VA, BASE_KV, ZONE, VMAX, VMIN, LAM_P, LAM_Q, MU_VMAX, MU_VMIN] = idx_bus;
+nb = size(mpc.bus, 1);
+slack = find(mpc.bus(:, BUS_TYPE) == REF);
+slack = slack(1);
+noref   = (2:nb)';
+noslack = find((1:nb)' ~= slack);
+Bbus = Bbus(noslack, noref);
+
 Bname = strcat(casename, '_Bbus.mtx');
 mmwrite(Bname, Bbus);
 %gzip(Bname, 'mtx');
